ee2d4c5925a88e27b4bb0f5ffc5275f174b1c1e0
Casey Langen <casey.langen@gmail.com>
2016-11-08 08:03:22 -0800

    Updated Formula's class name.

--- Formula/ProjectMMusikbox.rb
+++ Formula/ProjectMMusikbox.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class ProjectMMusikbox < Formula
+class Projectmmusikbox < Formula
     homepage 'http://musikcube.com'
     head 'git://github.com/clangen/projectM-musikcube.git'
     url 'https://github.com/clangen/projectM-musikcube/archive/0.1.tar.gz'
