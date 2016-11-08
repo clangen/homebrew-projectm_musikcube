099ada2208ed83912519d7cdcfbd88c429a76f48
Casey Langen <casey.langen@gmail.com>
2016-11-08 08:02:33 -0800

    Initial revision of the build file. May not work.

---
+++ Formula/ProjectMMusikbox.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class ProjectMMusikbox < Formula
+    homepage 'http://musikcube.com'
+    head 'git://github.com/clangen/projectM-musikcube.git'
+    url 'https://github.com/clangen/projectM-musikcube/archive/0.1.tar.gz'
+    sha256 'ee4afe8a952d5fa2bd5578d7362bdf7415514fd227ccc6a56496dab742eda3b8'
+    version '0.1'
+
+    depends_on 'cmake' => :build
+    depends_on 'musikbox'
+    depends_on 'glew'
+    depends_on 'freetype'
+    depends_on 'ftgl'
+    depends_on 'sdl2'
+
+    def install
+        system "cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=#{prefix} ."
+        system "make"
+        system "make install"
+    end
+end
