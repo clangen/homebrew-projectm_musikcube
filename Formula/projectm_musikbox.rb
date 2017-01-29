76a1466cb8094494ff2f92effa37bc5568e0f050
Casey Langen <casey.langen@gmail.com>
2017-01-28 23:27:19 -0800

    Updated to 0.3.1 (musikcube sdk v2)

--- Formula/projectm_musikbox.rb
+++ Formula/projectm_musikbox.rb
@@ -3,9 +3,9 @@ require 'formula'
 class ProjectmMusikbox < Formula
     homepage 'http://musikcube.com'
     head 'git://github.com/clangen/projectM-musikcube.git'
-    url 'https://github.com/clangen/projectM-musikcube/archive/0.3.0.tar.gz'
-    sha256 '0ad99a2286d322b46c8d8c61cb788188cc11d8646f3a060604363808b823cfec'
-    version '0.3.0'
+    url 'https://github.com/clangen/projectM-musikcube/archive/0.3.1.tar.gz'
+    sha256 '1ea8d69c24cce1a7f1c16765e934fc2b57e0eb8c4de9480af24e0f2cff122f34'
+    version '0.3.1'
 
     depends_on 'cmake' => :build
     depends_on 'musikbox'
