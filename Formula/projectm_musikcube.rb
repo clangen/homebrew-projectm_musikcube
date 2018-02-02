28942ed0585112d8b045eb57a0b1acda66089f49
Casey Langen <casey.langen@gmail.com>
2018-02-02 09:29:40 -0800

    Updated to 0.5.2

--- Formula/projectm_musikcube.rb
+++ Formula/projectm_musikcube.rb
@@ -3,9 +3,9 @@ require 'formula'
 class ProjectmMusikcube < Formula
     homepage 'http://musikcube.com'
     head 'git://github.com/clangen/projectM-musikcube.git'
-    url 'https://github.com/clangen/projectM-musikcube/archive/0.5.1.tar.gz'
-    sha256 '747ea1201d439ec9259eef8c580a88bb22c6181c98e6d00300699b7b48d29901'
-    version '0.5.1'
+    url 'https://github.com/clangen/projectM-musikcube/archive/0.5.2.tar.gz'
+    sha256 '4cb445512adeabf74a5a5e64a42c4a6eee53b652cdeb9b5431c69adcafc78aca'
+    version '0.5.2'
 
     depends_on 'cmake' => :build
     depends_on 'musikcube'
